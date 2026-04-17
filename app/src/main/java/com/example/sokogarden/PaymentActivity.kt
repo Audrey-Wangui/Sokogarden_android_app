5b781f012a313cae9f10c4adb4ff737bfd881a28
Audrey-Wangui <audreywangui1@gmail.com>
2026-04-17 10:23:09 +0300

    updated

---
+++ app/src/main/java/com/example/sokogarden/PaymentActivity.kt
@@ -0,0 +1,79 @@
+package com.example.sokogarden
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.loopj.android.http.RequestParams
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_payment)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+//        Find the views by use of their IDs
+        val txtname = findViewById<TextView>(R.id.txtProductName)
+        val txtcost = findViewById<TextView>(R.id.txtProductCost)
+        val imgProduct = findViewById<ImageView>(R.id.imgProduct)
+        val txtdescription = findViewById<TextView>(R.id.txtProductDescription)
+
+//        Retrive Data passed from the previous Activity
+        val name = intent.getStringExtra("product_name")
+        val cost = intent.getIntExtra("product_cost", 0)
+//        val description = intent.getStringExtra("Product_descrition")
+        val product_photo = intent.getStringExtra("product_photo")
+        val description = intent.getStringExtra("product_description")
+
+//        Update the TextView with the data passed from the previous Activity
+        txtname.text = name
+        txtcost.text = "Ksh $cost"
+        txtdescription.text = description
+
+
+//        Specify the image URL
+        val imageUrl = "https://audreywangui.alwaysdata.net/static/images/$product_photo"
+
+        Glide.with(this)
+            .load(imageUrl )
+            .placeholder(R.drawable.ic_launcher_background) // Make sure you have a placeholder image
+            .into(imgProduct)
+
+//        Find the EditText and the PAYNOW button by use of their IDs
+        val phone = findViewById<EditText>(R.id.phone)
+        val btnPay = findViewById<Button>(R.id.pay)
+
+//        Set click  listener on the button
+        btnPay.setOnClickListener {
+//            Specify the api endpoint for asking payment
+            val api = "https://audreywangui.alwaysdata.net/api/mpesa_payment"
+
+//            Create a RequestParams
+            val data = RequestParams()
+
+//            Insert data into the request params
+            data.put("amount", cost)
+            data.put("phone", phone)
+
+//            Import the helper class
+            val helper = ApiHelper(applicationContext)
+
+//            Access the post function inside of the helper class
+            helper.post(api, data)
+
+            phone.text.clear()
+        }
+
+    }
+}
